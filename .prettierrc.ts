8241c309aa7b526c637f69d66d91d9081bb266ea
gaoyi <194014849@qq.com>
2023-12-16 16:01:46 +0800

    first commit

---
+++ .prettierrc.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 130, // 超过最大值换行
+  tabWidth: 2, // 缩进字节数
+  useTabs: true, // 使用制表符而不是空格缩进行
+  semi: false, // 结尾不用分号(true有，false没有)
+  singleQuote: true, // 使用单引号(true单引号，false双引号)
+  bracketSpacing: true, // 在对象，数组括号与文字之间加空格 "{ foo: bar }"
+  jsxSingleQuote: true, // 在JSX中使用单引号而不是双引号
+  arrowParens: "avoid", //  箭头函数只有一个参数时是否要有小括号。avoid：无,always：有
+};
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>测 试</div>
+  <div>
+    <span> {{ a }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+var a = ref(3);
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
